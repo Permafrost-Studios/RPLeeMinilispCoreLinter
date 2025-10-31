250941fa49f5e1e011b2a2c896aec04f9dce0653
JulianKirk <JulianKirk2005@gmail.com>
2025-10-31 23:34:28 +1100

    wrote the lexer

--- Implementation.py
+++ Implementation.py
@@ -1,5 +1,12 @@
 from enum import Enum, auto
 
+class State(Enum):
+    START_OR_SPACE = auto()
+    NUMBER = auto()
+    IDENTIFIER = auto()
+    SINGLE_CHARACTER_TOKEN = auto()
+    ERROR = auto()
+
 class TokenType(Enum):
     NUMBER = auto()
     IDENTIFIER = auto()
@@ -15,10 +22,14 @@ class TokenType(Enum):
 
 
 class Token:
-
     def __init__(self, tokenType, value):
-        self.value = value
-        self.tokenType = tokenType
+        if tokenType is None:
+            # Single character tokens can nly have oone token type
+            self.tokenType = self.getTokenTypeForSingleCharacterToken(value)
+            self.value = value
+        else:
+            self.tokenType = tokenType
+            self.value = value
             
     def __str__(self):
         strings = {
@@ -34,7 +45,21 @@ class Token:
             TokenType.LPAREN: "(",
             TokenType.RPAREN: ")"
         }
-        return strings.get(self.type, None)
+        return strings.get(self.tokenType, None)
+    
+    def getTokenTypeForSingleCharacterToken(cls, character):
+        mapping = {
+            "+": TokenType.PLUS,
+            "−": TokenType.MINUS,
+            "×": TokenType.MULT,
+            "=": TokenType.EQUALS,
+            "?": TokenType.CONDITIONAL,
+            "λ": TokenType.LAMBDA,
+            "≜": TokenType.LET,
+            "(": TokenType.LPAREN,
+            ")": TokenType.RPAREN
+        }
+        return mapping.get(character, None)
 
     def __repr__(self):
         return str(self)
@@ -55,5 +80,106 @@ class Token:
         return not self == other
 
 
+class LexicalAnalyser:
+    @classmethod
+    def createTransitionFunctions(cls):
+        numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+        lowerCaseIdentifiers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        identifiers = lowerCaseIdentifiers + [x.upper() for x in lowerCaseIdentifiers]
+
+        operators = ["+", "×", "=", "−", "?", "λ", "≜"]
+        formattingCharacters = ["(", ")"]
+
+        singleCharacterTokenCharacters = operators + formattingCharacters
+
+        #alphabet = numbers + identifiers + singleCharacterTokenCharacters
+
+        allNonErrorStates = [State.START_OR_SPACE, State.NUMBER, State.IDENTIFIER, State.SINGLE_CHARACTER_TOKEN]
+
+        transitions = {}
+
+        for state in allNonErrorStates:
+            for digit in numbers:
+                transitions[(state, digit)] = State.NUMBER
+            
+            for letter in identifiers:
+                transitions[(state, letter)] = State.IDENTIFIER
+
+            for character in singleCharacterTokenCharacters:
+                transitions[(state, character)] = State.SINGLE_CHARACTER_TOKEN
+            
+            transitions[(state, " ")] = State.START_OR_SPACE
+
+        return transitions
+
+    @classmethod
+    def GetTokensForString(cls, input):
+        AcceptingStates = [State.NUMBER, State.IDENTIFIER]
+
+        transitionFunctionDictionary = cls.createTransitionFunctions()
+
+        allTokens = []
+
+        numberBuffer = ""
+        identifierBuffer = ""
+
+        currentState = State.START_OR_SPACE
+
+        for character in input:
+            newState = transitionFunctionDictionary.get((currentState, character), State.ERROR)
+
+            # print("Number Buffer: '" + numberBuffer + "'")
+            # print("Identifier Buffer: '" + identifierBuffer + "'")
+            # print("Transitioning from " + str(currentState) + " to " + str(newState) + " on character '" + character + "'")
+
+            if newState == State.ERROR:
+                raise Exception("Lexical Error: Character is not in the valid alphabet: '" + character + "'")
+            
+            if (newState != currentState):
+                if currentState == State.NUMBER:
+                    allTokens.append(Token(TokenType.NUMBER, int(numberBuffer)))
+                    numberBuffer = ""
+                elif currentState == State.IDENTIFIER:
+                    allTokens.append(Token(TokenType.IDENTIFIER, identifierBuffer))
+                    identifierBuffer = ""
+
+            currentState = newState
+
+            if currentState == State.SINGLE_CHARACTER_TOKEN:
+                allTokens.append(Token(None, character))
+            elif currentState == State.NUMBER:
+                numberBuffer += character
+            elif currentState == State.IDENTIFIER:
+                identifierBuffer += character
+
+        if currentState == State.NUMBER:
+            allTokens.append(Token(TokenType.NUMBER, int(numberBuffer)))
+        elif currentState == State.IDENTIFIER:
+            allTokens.append(Token(TokenType.IDENTIFIER, identifierBuffer))
+
+        return allTokens
+         
+def main():
+    print("test")
+
+    
 if __name__ == "__main__":
-     print("This is something")
+     # Basic expressions
+     print(LexicalAnalyser.GetTokensForString("42"))
+     print(LexicalAnalyser.GetTokensForString("x"))
+     print(LexicalAnalyser.GetTokensForString("(+ 2 3)"))
+     print(LexicalAnalyser.GetTokensForString("(× x 5)"))
+     
+     # Nested expressions
+     print(LexicalAnalyser.GetTokensForString("(+ (× 2 3) 4)"))
+     print(LexicalAnalyser.GetTokensForString("(? (= x 0) 1 0)"))
+     
+     # Function expressions
+     print(LexicalAnalyser.GetTokensForString("(λ x x)"))
+     print(LexicalAnalyser.GetTokensForString("(≜ y 10 y)"))
+     print(LexicalAnalyser.GetTokensForString("((λ x (+ x 1)) 5)"))
+     
+     # More complex
+     print(LexicalAnalyser.GetTokensForString("(× (+ 1 2) (− 5 3))"))
+     print(LexicalAnalyser.GetTokensForString("(λ f (λ x (f x)))"))
